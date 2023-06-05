0f70ed66bd329c80869783df490fc877ed292b2e
Will Perry <willpe@outlook.com>
2023-06-05 08:13:15 +0100

    Style fixes

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,7 +4,7 @@ import SpotifyClient from "../SpotifyClient";
 import { UserContext } from "../contexts/UserContext";
 import User from "../components/timeline/User";
 
-import { ReactComponent as Wordmark } from "../assets/logos/wordmark-logo.svg";
+import { ReactComponent as Wordmark } from "../assets/wordmark-logo.svg";
 
 export default function Root() {
   const spotifyClient = new SpotifyClient();
@@ -23,11 +23,9 @@ export default function Root() {
   return (
     <UserContext.Provider value={userContext}>
       <header>
-        <h1>
-          <a href="/" title="Home">
-            <Wordmark />
-          </a>
-        </h1>
+        <a className="logo" href="/" title="Home">
+          <Wordmark />
+        </a>
         <User />
       </header>
       <Outlet />
