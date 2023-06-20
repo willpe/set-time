b202a146f85a1db85ab50a84c0705fd6c70c5c7a
Will Perry <willpe@outlook.com>
2023-06-20 13:29:11 -0700

    Fix #20 - display festival list as cards

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,9 +3,33 @@ import { Link, useLoaderData } from "react-router-dom";
 export async function loader() {
   return {
     festivals: [
-      { name: "Electric Forest 2023", id: "electric-forest-2023" },
-      { name: "Mini Forest 2023", id: "mini-forest-2023" },
-      { name: "Explorations 2023", id: "explorations-2023" },
+      {
+        name: "Electric Forest 2023",
+        id: "electric-forest-2023",
+        startDate: "June 22, 2023",
+        endDate: "June 25, 2023",
+        location: {
+          name: "Rothbury, MI",
+        },
+      },
+      {
+        name: "Mini Forest 2023",
+        id: "mini-forest-2023",
+        startDate: "July 1, 2023",
+        endDate: "July 2, 2023",
+        location: {
+          name: "Washington",
+        },
+      },
+      {
+        name: "Explorations 2023",
+        id: "explorations-2023",
+        startDate: "June 6, 2023",
+        endDate: "June 14, 2023",
+        location: {
+          name: "Dhermi, Albania",
+        },
+      },
     ],
   };
 }
@@ -17,7 +41,13 @@ export default function Index() {
     <main className="list">
       <nav className="festival-list">
         {festivals.map((festival) => (
-          <Link to={`/${festival.id}`}>{festival.name}</Link>
+          <Link key={festival.id} to={`/${festival.id}`} className="festival-card">
+            <h3>{festival.name}</h3>
+            <p className="dates">
+              {festival.startDate} - {festival.endDate}
+            </p>
+            <p className="location">{festival.location.name}</p>
+          </Link>
         ))}
       </nav>
     </main>
