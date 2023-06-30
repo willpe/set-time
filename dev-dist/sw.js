65195ceda7d3ce11929555a9117db3bdcdad8c62
Will Perry <willpe@outlook.com>
2023-06-29 21:14:13 -0700

    Style Updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b7fccfec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ldf5v343vco"
+    "revision": "0.j45h4c71ai"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
